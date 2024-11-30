1076996fbd66c25af1eaf4938371ef5d9fd84754
devtushar001 <tc341269@gmail.com>
2024-11-30 09:11:16 -0500

    creating cart page

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -3,24 +3,42 @@ import './Cart.css';
 import { DochakiContext } from "../../components/Context/Contact";
 
 const Cart = () => {
-    const {cartItem, bikeAccessories, removeFromCart} = useContext(DochakiContext)
+    const { cartItem, bikeAccessories, removeFromCart } = useContext(DochakiContext);
+    console.log(cartItem, bikeAccessories);
+
     return (
         <>
-         <div className="cart">
-            <div className="cart-items">
-                <div className="cart-item-items-title">
-                    <p>Items</p>
-                    <p>Title</p>
-                    <p>Price</p>
-                    <p>Quantity</p>
-                    <p>Total</p>
-                    <p>Remove</p>
+            <div className="cart">
+                <div className="cart-items">
+                    <div className="cart-item-items-title">
+                        <p>Items</p>
+                        <p>Title</p>
+                        <p>Price</p>
+                        <p>Quantity</p>
+                        <p>Total</p>
+                        <p>Remove</p>
+                    </div>
+                    <br />
+                    <hr />
+                    {bikeAccessories.map((item, i) => {
+                        if (cartItem[item.id] > 0) {
+                            return (
+                                <>
+                                  <div className="cart-items-title cart-items-item">
+                                    <img src={item.images.mainImage} alt="" />
+                                    <p>{item.name}</p>
+                                    <p>{item.price.newPrice}</p>
+                                    <p>{cartItem[item.id]}</p>
+                                    <p></p>
+                                    <p></p>
+                                  </div>
+                                </>
+                            )
+
+                        }
+                    })}
                 </div>
-                <br />
-                <hr />
-                
             </div>
-         </div>
         </>
     )
 }
