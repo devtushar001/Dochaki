5beec7f049f09d9aa1f10d275ef8901f2df2f7b0
devtushar001 <tc341269@gmail.com>
2024-12-06 09:34:44 -0500

    Some images are deleted

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -8,7 +8,7 @@ import Orders from "./pages/Orders/Orders";
 import Panel from "./pages/Panel/Panel";
 // import NewAdd from "./pages/NewAdd.jsx/NewAdd";
 import { ToastContainer } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
@@ -16,18 +16,18 @@ function App() {
 
   return (
     <>
-    <ToastContainer />
       <Navbar />
       <hr />
       <div className="app-content">
         <Sidebar />
         <Routes>
           <Route path="/" element={<Panel url={url} />} />
-          <Route path="/add" element={<Add url={url}/>} />
+          <Route path="/add" element={<Add url={url} />} />
           <Route path="/list" element={<List url={url} />} />
           <Route path="/orders" element={<Orders url={url} />} />
           {/* <Route path="/new-add" element={<NewAdd />} /> */}
         </Routes>
+        <ToastContainer />
       </div>
     </>
   )
